ed7acf23caa77135280caebbcff840e0252c5aee
Birasa Divine <b.divine@alustudent.com>
2026-05-10 23:30:47 +0200

    Add User model to define user details in the database

---
+++ app/models/user.py
@@ -0,0 +1,9 @@
+from database import Base
+from sqlalchemy import Column , Integer , String
+class User(Base):
+    __tablename__= "users"
+    
+    id = Column(Integer , primary_key=True , index=True)
+    name = Column (String(100) , nullable= False)
+    email = Column (String(100) , nullable=False , unique=True)
+    password= Column(String(100) , nullable=False)
