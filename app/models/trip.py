d3698cae3fbbe20c244cc44b6d7cc6e1c2b91791
Birasa Divine <b.divine@alustudent.com>
2026-05-10 23:30:41 +0200

    Add Trip model to define trip details in the database

---
+++ app/models/trip.py
@@ -0,0 +1,12 @@
+from database import Base
+from sqlalchemy import Column , Integer , String , ForeignKey
+
+
+class Trip(Base):
+    __tablename__= "trip"
+    user_id = Column (Integer , ForeignKey("users.id"), nullable= False , index=True)
+    id = Column (Integer , primary_key= True , index=True)
+    destination = Column(String(100) , nullable= False) 
+    days = Column(Integer , nullable= False) 
+    budget =Column(Integer , nullable= False) 
+    trip_style = Column(String(100) , nullable= False) 
