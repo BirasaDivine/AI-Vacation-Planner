80666c10c67802b0a10fb65b19f290122ffc56c2
Birasa Divine <b.divine@alustudent.com>
2026-06-13 18:49:46 +0200

    Add Claude integration for chat functionality

---
+++ app/claude.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from anthropic import Anthropic
+import os
+import json
+load_dotenv()
+client=Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+model='claude-haiku-4-5-20251001'
+
+def add_user_message(messages,text):
+    user_message={"role": "user" , "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages,text):
+    assistant_message={"role": "assistant" , "content": text}
+    messages.append(assistant_message)
+
+def chat(messages):
+    message=client.messages.create(
+        model=model ,
+        max_tokens=1000,
+        messages=messages,
+    )
+    return message.content[0].text # type: ignore
+
